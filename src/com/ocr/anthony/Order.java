775e566eed10fa59d882f31dc44868d4b8abc720
ManuelAntunes <manuel@antunes.me>
2019-02-09 00:07:16 +0100

    menu créé test ok expérimentation 1ere concaténation

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,13 +5,20 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+        System.out.println("choix Menu");
+        System.out.println("1 - Poulet");
+        System.out.println("2 - Boeuf");
+        System.out.println("3 - Vegetarien");
+        System.out.println("Que souhaitez vous comme menu ?");
     }
+
+
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
+        System.out.println("Vous avez choisi le menu " + nbMenu);
 
     }
 }
